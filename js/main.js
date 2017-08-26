82c3016797e989d3988eaee6ca1db435ba3d0c2b
jbmarflo <jbmarflo@gmail.com>
2017-08-26 16:35:48 -0500

    mensaje de enviado, checkbox y algunos detalles

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
 		messagingSenderId: "44910618993"
 	    };
 	    firebase.initializeApp(config);
-	 
-	    var carrera_estudiar; 	
 
-	$('input[name="group1"]').on('change', function(){
+    var carrera_estudiar;
+
+    $('input[name="group1"]').on('change', function(){
 		var value = $(this).val();
 		if (value == 1) {
 		$('.description_what_study').removeClass('hide');
@@ -26,18 +26,21 @@ $(document).ready(function() {
 
 	$('form#form').on('submit', function(e){
 		e.preventDefault();
-		var carrera = $("#carrera").val(); 
+		var carrera = $("#carrera").val();
 		var data_studio = {sabe_que_estudiar:carrera_estudiar,carrera:carrera};
 		var cursos_de_interes = $("#cursos").val();
 		var whatsapp = $("#whatsapp").val();
 		var email = $("#email").val();
-		
-		var data = {whatsapp:whatsapp,email:email,Quiere_Estudiar:data_studio,cursos_de_interes:cursos_de_interes};	
-		
-		var name = $("#nameUser").val();		
+
+		var data = {whatsapp:whatsapp,email:email,Quiere_Estudiar:data_studio,cursos_de_interes:cursos_de_interes};
+
+		var name = $("#nameUser").val();
 		var database = firebase.database();
 		var ref = database.ref("Users").child(name);
 		console.log(data);
 		ref.set(data);
+
+		$(this).hide();
+		$('.success-text').show();
 	});
 });
