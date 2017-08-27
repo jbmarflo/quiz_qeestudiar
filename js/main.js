62143a7962dd53fb073c9cec0bea57ecd2a93598
sienrik15 <enrique_sam11@hotmail.com>
2017-08-27 09:17:47 -0500

    nuevas img
    cambios de img, firebase. validacion.

--- js/main.js
+++ js/main.js
@@ -28,11 +28,33 @@ $(document).ready(function() {
 		e.preventDefault();
 		var carrera = $("#carrera").val();
 		var data_studio = {sabe_que_estudiar:carrera_estudiar,carrera:carrera};
+
+		var office =$("#office").is(':checked');
+		var building = $("#building").is(':checked');
+		var hospital = $("#hospital").is(':checked');
+		var tv = $("#tv").is(':checked');
+		var laboratory = $("#laboratory").is(':checked');
+		var technology = $("#technology").is(':checked');
+		var class_room = $("#class_room").is(':checked');
+		var art = $("#art").is(':checked');
+		var car = $("#car").is(':checked');
+		var nature = $("#nature").is(':checked');
+
+		var imgData = {office:office, building:building, hospital:hospital, tv:tv, laboratory:laboratory,
+		technology:technology, class_room:class_room,art:art,car:car,nature:nature}
 		var cursos_de_interes = $("#cursos").val();
+		
+		var streaming = $("#streaming").val();
+		var buscador = $("#buscador").val();
+		var beneficios = $("#beneficios").val();
+
+		var saluciones = {streaming:streaming,buscador:buscador,beneficios:beneficios}
+
 		var whatsapp = $("#whatsapp").val();
 		var email = $("#email").val();
+	
 
-		var data = {whatsapp:whatsapp,email:email,Quiere_Estudiar:data_studio,cursos_de_interes:cursos_de_interes};
+		var data = {whatsapp:whatsapp,email:email,Quiere_Estudiar:data_studio,cursos_de_interes:cursos_de_interes,carrera_de_Interes:imgData,saluciones:saluciones};
 
 		var name = $("#nameUser").val();
 		var database = firebase.database();
